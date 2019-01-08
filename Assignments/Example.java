f5b7a73b71b07c71ede73bb28f2b7bdddb4688aa
Halle Wong <Wong.Hall2020@ssusd.org>
2019-01-08 07:28:15 -0800

    adds Cruise Control

---
+++ Assignments/Example.java
@@ -0,0 +1,29 @@
+class Example {
+  
+  static Boolean isEven(int x) {
+        return x % 2 == 0;
+    }
+  static String print(String message) {
+        String == "Hello"
+    }  
+
+  public static void main(String args[]) {
+        
+
+        if( isEven(x) ) {
+            System.out.println("X is Even.");
+        }
+        else {
+            System.out.println("X is Odd.");
+        }
+    }
+    public static Boolean isEven(int x) {
+        return x % 2 == 0;
+    }  
+    
+    public static Boolean isEven(int x) {
+        return x % 2 == 0;
+    }  
+
+
+}
